74a6588d408c76bc598dae3d7d3208c901bd60d9
alifrashid00 <asiforrashid@iut-dhaka.edu>
2024-05-04 01:58:14 +0600

    file sharing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,23 @@ import { Routes, Route } from "react-router-dom";
 import {useAuthContext} from "./context/AuthContext.jsx";
 import {Toaster} from "react-hot-toast";
 import {Navigate} from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+
+import {Fa0} from "react-icons/fa6";
+import FileSharing from "./pages/Filesharing/FileSharing.jsx";
+
+
 
 function App() {
     const { authUser } = useAuthContext();
     return (
         <div className='p-4 h-screen flex items-center justify-center'>
             <Routes>
-                <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+                <Route path='/' element={authUser ? <Dashboard/> : <Navigate to={"/login"} />} />
                 <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
                 <Route path='/signup' element={authUser ? <Navigate to='/' /> : <Signup />} />
+                <Route path='/filesharing' element={authUser ? <FileSharing/> : <Navigate to={"/login"} />} />
+                <Route path='/message' element={authUser ? <Home/> : <Navigate to={"/login"} />} />
             </Routes>
             <Toaster/>
         </div>
